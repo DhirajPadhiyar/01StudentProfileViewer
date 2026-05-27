3834f2d83bad004d054647c0b432850178b6395e
dmpadhiyar0705@outlook.com <dmpadhiyar0705@outlook.com>
2026-05-27 16:47:16 +0530

    Add project files.

---
+++ 01StudentProfileViewer/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using _01StudentProfileViewer.Models;
+
+namespace _01StudentProfileViewer.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            Student student = new Student()
+            {
+                Name = "Dhiraj Padhiyar",
+                Age = 30,
+                City = "Dubai",
+                Course = "ASP.NET Core MVC"
+            };
+
+            Student student1 = new Student()
+            {
+                Name = "Ravi Padhiyar",
+                Age = 32,
+                City = "New York City",
+                Course = "OPTIONS TRADING"
+            };
+            List<Student> students = new List<Student>()
+            {
+                 student,
+                 student1
+            };
+
+            return View(students);
+        }
+    }
+}
---
+++ 01StudentProfileViewer/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace _01StudentProfileViewer.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Course { get; set; }
+    }
+}
